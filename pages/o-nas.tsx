caea429f5d727c364e75843c79ed08369c3874ec
Daniel Hasek <hasek.dan1@gmail.com>
2023-01-31 12:07:14 +0100

    .gitignore fixed

--- pages/o-nas.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../components/Layout/Layout";
-
-const about = () => {
-  return <Layout>xfdsa</Layout>;
-};
-
-export default about;
--- utils/formatCurrency.ts
+++
@@ -1,3 +0,0 @@
-export const formatCurrency = (currency: number) => {
-  return currency.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-};
